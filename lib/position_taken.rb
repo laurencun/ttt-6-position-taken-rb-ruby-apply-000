877dc4ba5b501b9ae1516ab349bdd5a87630ee66
Lauren Cunningham <lauren.busbee@gmail.com>
2020-05-16 04:46:23 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,7 +1,7 @@
 # code your #position_taken? method here!
 position_taken?(board, index)
-  if board[index] == " "|| board[index] == "" || board[index] == nil
+  if (board[index] == " "|| board[index] == "" || board[index] == nil)
     return false
-  else board[index] != " " || "" || nil
+  else !(board[index] == " " || board[index] == "" || board[index] == nil)
     return true
 end

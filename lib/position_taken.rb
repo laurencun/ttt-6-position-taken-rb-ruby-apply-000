3e90431195b6a534609efa5a5a69db5ebad1500d
Lauren Cunningham <lauren.busbee@gmail.com>
2020-05-16 04:28:14 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,4 +1,8 @@
 # code your #position_taken? method here!
 position_taken?(board, index)
-  if index = " " || index = ""
+  if array[index_number] == " "
     return false
+  else array[index_number] != " "
+    return true
+  end
+end  
